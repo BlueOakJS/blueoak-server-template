1ab64f7a834a49887a63fb4105d81d97da52d60b
Sean Kennedy <sean.kennedy@pointsource.com>
2016-04-08 16:52:41 -0400

    Update dependencies, get the full BOS test utilities for the empty test

--- test/unit/test.js
+++ test/unit/test.js
@@ -1,5 +1,5 @@
-var testUtil = require('blueoak-server').testUtility;
+var testUtil = require('blueoak-server').testUtility();
 
 describe('Empty test', function () {
-
+	process.stdout.write('BlueOak Server test utility functions:\n- ' + Object.keys(testUtil).join('\n- '));
 });
